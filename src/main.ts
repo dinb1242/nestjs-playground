25512f81e488747c5d89c6d5a5e60a20e0fcce5a
Jihyun, Jeong <dinb1242@naver.com>
2025-03-22 19:30:16 +0900

    feat: Sample health check (추후 env 에 따른 주소로 변경)

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,20 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('apidoc', app, documentFactory);
 
+  /**
+   * Enable shutdown hooks
+   *
+   * NOTE:
+   *  enableShutdownHooks consumes memory by starting listeners.
+   *  In cases where you are running multiple Nest apps in a single Node process (e.g., when running parallel tests with Jest),
+   *  Node may complain about excessive listener processes.
+   *  For this reason, enableShutdownHooks is not enabled by default.
+   *  Be aware of this condition when you are running multiple instances in a single Node process.
+   *
+   *                                                                                quoted from NestJS Docs
+   */
+  app.enableShutdownHooks();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
