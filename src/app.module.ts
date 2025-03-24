74ec9f44f30700e86ece1f7dcf0b300bfc580782
Jihyun, Jeong <dinb1242@naver.com>
2025-03-24 21:55:52 +0900

    feat: ConfigModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './core/health/health.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
