de27fb838609041b37518151184974cb40997992
Jihyun, Jeong <dinb1242@naver.com>
2025-03-24 20:47:13 +0900

    feat: Health Check 추가 (MemHeap, MemRSS)

--- src/core/health/health.controller.ts
+++ src/core/health/health.controller.ts
@@ -3,6 +3,7 @@ import {
   HealthCheck,
   HealthCheckService,
   HttpHealthIndicator,
+  MemoryHealthIndicator,
 } from '@nestjs/terminus';
 
 @Controller('health')
@@ -10,13 +11,16 @@ export class HealthController {
   constructor(
     private readonly health: HealthCheckService,
     private readonly http: HttpHealthIndicator,
+    private readonly memory: MemoryHealthIndicator,
   ) {}
 
   @Get()
   @HealthCheck()
   async check() {
     return this.health.check([
-      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+      () => this.http.pingCheck('server', 'http://localhost:3000'),
+      () => this.memory.checkHeap('memory-heap', 150 * 1024 * 1024),
+      () => this.memory.checkRSS('memory-rss', 150 * 1024 * 1024),
     ]);
   }
 }
